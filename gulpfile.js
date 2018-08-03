97dce57d668100a7c8a1fdb66c2b28366c245436
manno-k <art.kuromi@gmail.com>
2018-08-03 23:57:10 +0900

    gulpfile更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,207 +1,176 @@
+
 /*
  Destinations
  */
 
-var gulp = require('gulp'),
-    gulpLoadPlugins = require('gulp-load-plugins'),
-    $ = gulpLoadPlugins();
+gulp = require('gulp'),
+	gulpLoadPlugins = require('gulp-load-plugins'),
+	$ = gulpLoadPlugins();
 
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var cssnext = require('postcss-cssnext');
-var browserSync = require('browser-sync');
-var runSequence = require('run-sequence');
-var sourcemaps = require('gulp-sourcemaps');
-var template = require('gulp-template');
-var styleguide = require('sc5-styleguide');
-var ejs = require("gulp-ejs");
-var rename = require("gulp-rename");
-var fs = require('fs');
-
+sass = require('gulp-sass');
+postcss = require('gulp-postcss');
+cssnext = require('postcss-cssnext');
+browserSync = require('browser-sync');
+runSequence = require('run-sequence');
+sourcemaps = require('gulp-sourcemaps');
+template = require('gulp-template');
+styleguide = require('sc5-styleguide');
+ejs = require("gulp-ejs");
+rename = require("gulp-rename");
+svgmin = require("gulp-svgmin");
+fs = require('fs');
+concat = require("gulp-concat");
+watch = require('glob-watcher');
 
 
 /*
  File Destinations
  */
 
-var path = {
-    'root': './',
-    // images
-    'imageDest': 'build/assets/img/',
-    'imagePath': 'src/img/**/*',
-    // SVG
-    // JS
-    'jsDest': 'build/assets/js/',
-    'jsPath': 'src/js/**/*.js',
-    // Sass
-    'sassPass': 'src/sass/**/*.scss',
-    'sassIgnore': '!src/sass/',
-    // Css
-    'cssDest': 'build/assets/css/',
-    // EJS
-    'ejsDest': 'build/',
-    'ejsConfig': './src/ejs/config.json',
-    //Souce map
-    'souceMapDest': './',
-    // Style guide
-    'styleGuideDest': 'doc/styleguide/',
-    'styleGuideOverview': 'doc/styleguide/styleguide.md',
-    'styleGuideName': '',
-    'styleGuidePort': '4000'
+path = {
+	'root': './',
+	// images
+	'imageDest': 'build/assets/img/',
+	'imagePath': 'src/img/**/*',
+	// SVG
+	// JS
+	'jsDest': 'build/assets/js/',
+	'jsPath': 'src/js/**/*.js',
+	'jsConcatDist': 'script.js',
+	'jsConcatDistPath': 'src/js/',
+	// Sass
+	'sassPass': 'src/sass/**/*.scss',
+	'sassIgnore': '!src/sass/',
+	// Css
+	'cssDest': 'build/assets/css/',
+	// EJS
+	'ejsDest': 'build/',
+	'ejsConfig': './src/ejs/config.json',
+	//Souce map
+	'souceMapDest': './',
+	// Style guide
+	'styleGuideDest': 'doc/styleguide/',
+	'styleGuideOverview': 'doc/styleguide/styleguide.md',
+	'styleGuideName': '',
+	'styleGuidePort': '4000'
 };
 
 var sassLintConf = {
-    configFile: '.sass-lint.yml'
+	configFile: '.sass-lint.yml'
 };
-
-/*
- Js Tasks
- */
-
-gulp.task('uglify', function () {
-    var eachscript = gulp.src(path.jsPath)
-        .pipe($.plumber())
-        .pipe($.uglify())
-        .pipe($.rename({
-            extname: '.min.js'
-        }))
-        .pipe(gulp.dest(path.jsDest));
-});
-
-gulp.task("concat", function () {
-    return gulp.src([
-        '',
-        ''
-    ])
-        .pipe($.plumber())
-        .pipe($.concat("minified.js"))
-        .pipe(gulp.dest(path.jsDest));
-});
-
 /*
  Sass Tasks
  */
 
-gulp.task('scss', function (){
-    var processors = [
-        cssnext({
-            browsers: ['last 2 versions', 'ie 10', 'ie 9', 'iOS >= 7', 'Android >= 4.2'],
-            features: {
-                customProperties: true,
-                calc: true,
-                customMedia: true,
-                mediaQueriesRange: false
-            }
-        })
-    ];
-    return gulp.src(path.sassPass)
-        .pipe($.plumber({
-            errorHandler: function (err) {
-                console.log(err.messageFormatted);
-                this.emit('end');
-            }
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
-        // テーマフォルダのルートにsourcemapを生成する
-        .pipe(sourcemaps.write(path.souceMapDest))
-        .pipe(gulp.dest(path.cssDest))
+gulp.task('scss', function () {
+	var processors = [
+		cssnext({
+			browsers: ['last 2 versions', 'ie 10', 'ie 9', 'iOS >= 7', 'Android >= 4.2'],
+			features: {
+				customProperties: true,
+				calc: true,
+				customMedia: true,
+				mediaQueriesRange: false
+			}
+		})
+	];
+	return gulp.src(path.sassPass)
+	.pipe($.plumber({
+		errorHandler: function (err) {
+			console.log(err.messageFormatted);
+			this.emit('end');
+		}
+	}))
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(postcss(processors))
+	// テーマフォルダのルートにsourcemapを生成する
+	.pipe(sourcemaps.write(path.souceMapDest))
+	.pipe(gulp.dest(path.cssDest))
 
-});
-
-
-// メディアクエリを最適化
-gulp.task('combineMq', function () {
-    return gulp.src(path.cssDest + 'style.css')
-        .pipe($.combineMq({
-            beautify: false
-        }))
-        .pipe(gulp.dest(path.cssDest));
 });
 
 
 // cssの圧縮
 gulp.task('cssmin', function () {
-    gulp.src(path.cssDest + 'style.css')
-        .pipe($.cssmin())
-        .pipe($.rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest(path.cssDest));
+	return gulp.src(path.cssDest + 'style.css')
+	.pipe($.cssmin())
+	.pipe($.rename({
+		extname: '.min.css'
+	}))
+	.pipe(gulp.dest(path.cssDest))
+	.pipe(browserSync.stream());
+
 });
 
 /*
- Style Guide Tasks
+ Js Tasks
  */
 
-gulp.task('styleguide:generate', function () {
-    return gulp.src(path.sassPass)
-        .pipe(styleguide.generate({
-            port: path.styleGuidePort,
-            title: path.styleGuideName,
-            server: true,
-            rootPath: path.styleGuideDest,
-            overviewPath: path.styleGuideOverview,
-        }))
-        .pipe(gulp.dest(path.styleGuideDest));
-});
+gulp.task('script' ,function () {
+	return gulp.src(
+		[
+			'',
+			'',
+			'',
 
-gulp.task('styleguide:applystyles', function () {
-    return gulp.src(path.sassPass)
-        .pipe($.sass({
-            errLogToConsole: true
-        }))
-        .pipe(styleguide.applyStyles())
-        .pipe(gulp.dest(path.styleGuideDest));
+		]
+	).pipe($.plumber())
+	.pipe(concat(path.jsConcatDist))
+	.pipe($.uglify())
+	.pipe($.rename({
+		extname: '.min.js'
+	}))
+	.pipe(gulp.dest(path.jsDest))
+	.pipe(browserSync.reload({ stream: true }));
 });
-
 /*
  Image Tasks
  */
 
 gulp.task('image-opt', function () {
-    return gulp.src(path.imagePath)
-        .pipe($.plumber())
-        .pipe($.image({
-            pngquant: true,
-            optipng: true,
-            zopflipng: true,
-            advpng: true,
-            jpegRecompress: false,
-            jpegoptim: false,
-            mozjpeg: true,
-            gifsicle: true,
-            svgo: false,
-            concurrent: 10,
-        }))
-        .pipe(gulp.dest(path.imageDest));
+	return gulp.src(path.imagePath)
+	.pipe($.plumber())
+	.pipe($.image({
+		pngquant: true,
+		optipng: true,
+		zopflipng: true,
+		advpng: true,
+		jpegRecompress: false,
+		jpegoptim: false,
+		mozjpeg: true,
+		gifsicle: true,
+		svgo: false,
+		concurrent: 10,
+	}))
+	.pipe(gulp.dest(path.imageDest));
 });
 
 
 gulp.task('svgmin', function () {
-    gulp.src(path.imagePath + '**/*.svg')
-        .pipe($.svgmin())
-        .pipe(gulp.dest(path.imageDest));
+	return gulp.src(path.imagePath + '**/*.svg')
+	.pipe(svgmin())
+	.pipe(gulp.dest(path.imageDest));
 });
+
 
 /*
   EJS Tasks
  */
 
 gulp.task("ejs", function () {
-    json = JSON.parse(fs.readFileSync(path.ejsConfig));
-    gulp.src(["src/ejs/**/*.ejs", '!' + "src/ejs/**/_*.ejs"])
-        .pipe($.plumber({
-            errorHandler: function (err) {
-                console.log(err.messageFormatted);
-                this.emit('end');
-            }
-        }))
-        .pipe(ejs(json,{"ext": ".html"}))
-        .pipe(rename({extname: ".html"}))
-        .pipe(gulp.dest(path.ejsDest))
+	json = JSON.parse(fs.readFileSync(path.ejsConfig));
+	return gulp.src(["src/ejs/**/*.ejs", '!' + "src/ejs/**/_*.ejs"])
+	.pipe($.plumber({
+		errorHandler: function (err) {
+			console.log(err.messageFormatted);
+			this.emit('end');
+		}
+	}))
+	.pipe(ejs(json, {"ext": ".html"}))
+	.pipe(rename({extname: ".html"}))
+	.pipe(gulp.dest(path.ejsDest))
 });
 
 
@@ -210,15 +179,23 @@ gulp.task("ejs", function () {
  */
 
 gulp.task('browser-sync', function () {
-    browserSync({
-        server: {
-            baseDir: "./build/",
-            directory: false
-        }
-    });
+	return browserSync({
+		server: {
+			baseDir: "./build/",
+			directory: false
+		}
+	});
 });
+
 gulp.task('bs-reload', function () {
-    return browserSync.reload();
+	browserSync.reload();
+});
+gulp.task('watch', function () {
+	gulp.watch(path.root + "**/*.html").on('change', browserSync.reload);
+	gulp.watch(path.root + "**/*.ejs", gulp.series('ejs')).on('change', browserSync.reload);
+	gulp.watch(path.jsPath, gulp.series('script'));
+	gulp.watch(path.sassPass, gulp.series('scss', 'cssmin' ));
+
 });
 
 
@@ -227,48 +204,23 @@ gulp.task('bs-reload', function () {
 //---------------------------------------------------------------------------
 // default
 
-gulp.task('default', ['browser-sync'], function () {
-    gulp.watch(path.root + "**/*.html", ['bs-reload']);
-    gulp.watch(path.root + "**/*.ejs", function () {
-        return runSequence(
-            'ejs',
-            'bs-reload'
-        );
-    });
-    gulp.watch(path.sassPass, function () {
-        return runSequence(
-            'scss',
-            // 'combineMq',
-            'cssmin',
-            'bs-reload'
-        );
-    });
-    gulp.watch(path.jsPath, function () {
-        return runSequence(
-            'uglify',
-            'concat',
-            'bs-reload'
-        );
-    });
-});
+gulp.task('default', gulp.series(
+	gulp.parallel(
+		'browser-sync',
+		'watch',
+	)
+));
 
 // 画像圧縮
-gulp.task('image', function () {
-    return runSequence(
-        'image-opt',
-        'svgmin'
-    );
-});
+
+gulp.task('image', gulp.series(
+	gulp.parallel('image-opt', 'svgmin')
+));
 
 // sass lint task
 gulp.task('test-sass', function () {
-    return gulp.src([path.sassPass, path.sassIgnore + 'style.scss', path.sassIgnore + 'foundation/bootstrap/**/*.scss', path.sassIgnore + 'foundation/fontawesome/**/*.scss'])
-        .pipe($.sassLint())
-        .pipe($.sassLint.format())
-        .pipe($.sassLint.failOnError())
-});
-
-// Styleguide 生成
-gulp.task('style', ['styleguide:generate', 'styleguide:applystyles'], function () {
-    gulp.watch(path.sassPass, ['styleguide:generate', 'styleguide:applystyles', 'bs-reload']);
+	return gulp.src([path.sassPass, path.sassIgnore + 'style.scss', path.sassIgnore + 'foundation/bootstrap/**/*.scss', path.sassIgnore + 'foundation/fontawesome/**/*.scss'])
+	.pipe($.sassLint())
+	.pipe($.sassLint.format())
+	.pipe($.sassLint.failOnError())
 });
